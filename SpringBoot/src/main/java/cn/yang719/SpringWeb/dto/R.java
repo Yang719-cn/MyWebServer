95e5f633387d4f493adde2270fe59c77113202fb
yanghongyu <1427023844@qq.com>
2022-08-22 14:36:38 +0800

    摸

--- SpringBoot/src/main/java/cn/yang719/SpringWeb/dto/R.java
+++ SpringBoot/src/main/java/cn/yang719/SpringWeb/dto/R.java
@@ -2,6 +2,57 @@ package cn.yang719.SpringWeb.dto;
 
 
 public class R<T> {
+    /**
+     *状态码
+     */
+    private int code;
+
+    /**
+     * 返回文本消息
+     */
+    private String msg;
+
+    /**
+     * 返回是否执行成功
+     */
+    private  boolean success;
+
+    /**
+     *
+     */
+    private T data;
+
+    private Result result;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void data(T data){
+        this.data = data;
+    }
+
+    /**
+     * 构造器私有化，防止外界任意创建对象
+     */
+    private R(){}
+
+    public R success(boolean success){
+        this.success = success;
+        return this;
+    }
 
 
 }
