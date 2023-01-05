0506ef2cc73591dae21af0511806ebc2458578df
yanghongyu <1427023844@qq.com>
2023-01-05 22:13:06 +0800

    完善了如果没有获取到图片文件的判断

--- SpringBoot/src/main/java/cn/yang719/SpringWeb/utils/file/image/ImageGet.java
+++ SpringBoot/src/main/java/cn/yang719/SpringWeb/utils/file/image/ImageGet.java
@@ -1,6 +1,8 @@
 package cn.yang719.SpringWeb.utils.file.image;
 
+import cn.yang719.SpringWeb.dto.Result;
 import cn.yang719.utils.image.Base64Tool;
+import cn.yang719.utils.image.ImgsBase64;
 import org.apache.tomcat.util.codec.binary.Base64;
 
 import java.io.*;
@@ -8,11 +10,18 @@ import java.io.*;
 public class ImageGet {
 
     //java读取本地文件再将其转换为base64发到前端
-    public static String getBase64(String url) throws IOException {
-        return Base64Tool.imgToBase64(url);
+    public static Result getBase64(String url) throws IOException {
+        String str;
+        try {
+            str = Base64Tool.imgToBase64(url);
+            return Result.ok().data(str);
+        }catch (IOException e){
+            e.printStackTrace();
+            return Result.fail().msg("未获取到图片").data(ImgsBase64.failToGetImg);
+        }
     }
 
-    public static void imgDownload(){
-
+    public static Result imgDownload(){
+        return Result.fail().msg("未完工");
     }
 }
