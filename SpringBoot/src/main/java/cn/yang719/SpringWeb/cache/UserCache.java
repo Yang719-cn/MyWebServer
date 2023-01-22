fee7cf2fb847c87da3b4688a745da76dd26bd9bd
Yang719 <1427023844@qq.com>
2023-01-22 20:21:42 +0800

    用于验证用户的cache缓存，为后面删除用户操作判断以及免输入密码铺路。

--- SpringBoot/src/main/java/cn/yang719/SpringWeb/cache/UserCache.java
+++ SpringBoot/src/main/java/cn/yang719/SpringWeb/cache/UserCache.java
@@ -1,6 +1,13 @@
 package cn.yang719.SpringWeb.cache;
 
+import cn.yang719.SpringWeb.entity.User;
+import org.springframework.cache.annotation.Cacheable;
+
 public class UserCache {
 
+    @Cacheable()
+    public String getEmailCode(User user){
+        return null;
+    }
 
 }
